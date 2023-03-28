c76cd43450aee9aca51371a30a51ac1d78aa74b5
AndreyTarkaniy <endrubass@gmail.com>
2023-03-28 18:52:19 +0300

    add main code

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import { createListMarkupCountry } from './js/createListMarcupCountry';
+import { createListMarkupCountryInfo } from './js/createListMarcupCountryInfo';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+
+const DEBOUNCE_DELAY = 300;
+
+refs = {
+  input: document.querySelector('#search-box'),
+  listCountry: document.querySelector('.country-list'),
+  infoCountry: document.querySelector('.country-info'),
+};
+
+const onFetchInfo = () =>
+  Notify.info('Too many matches found. Please enter a more specific name.');
+
+const onFetchError = () =>
+  Notify.failure('Oops, there is no country with that name');
+
+const renderCountry = country => {
+  if (country.length > 10) {
+    onFetchInfo();
+  } else if (country.length === 1) {
+    const markup = createListMarkupCountryInfo(country);
+    refs.infoCountry.innerHTML = markup;
+  } else {
+    const markup = createListMarkupCountry(country);
+    refs.listCountry.innerHTML = markup;
+  }
+};
+
+const handleSearchCountry = event => {
+  event.preventDefault();
+
+  const searchCountry = event.target.value.trim();
+
+  if (searchCountry) {
+    fetchCountries(searchCountry).then(renderCountry).catch(onFetchError);
+  }
+
+  refs.listCountry.innerHTML = '';
+  refs.infoCountry.innerHTML = '';
+};
+
+refs.input.addEventListener(
+  'input',
+  debounce(handleSearchCountry),
+  DEBOUNCE_DELAY
+);
